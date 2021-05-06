62cb54d9539a9d1f5c5322d70f5fffa8946c6dc0
ivannofick <ivannofick@gmail.com>
2021-05-07 02:17:45 +0700

    add tts

--- src/app/views/queue/queue.component.ts
+++ src/app/views/queue/queue.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import Speech from 'speak-tts' // es6
 @Component({
   selector: 'app-queue',
   templateUrl: './queue.component.html',
@@ -24,37 +24,23 @@ export class QueueComponent implements OnInit {
     private router: Router
   ) { }
 
-  ngOnInit(): void {
-    this.loadData();
-  }
+  ngOnInit(): void {}
 
   changePage(path) {
     this.router.navigate(['/' + path]);
   }
 
 
-  loadData() {
-    this.data = [
-      {
-        'site'          : 'gudang',
-        'order'         : 'jjnGY75FTf',
-        'customer_id'   : 'njkGF7tR',
-        'customer_name' : 'Budi Nugroho',
-        'description'   : 'Pembelian Sweater ',
-        'price'         : '200000',
-      }
-    ]
+  openKarcis(event) {
+    console.log('masuk');
+    const speech = new Speech()
+    speech.speak({
+        text: 'Mas Ipan',
+    }).then(() => {
+        console.log("Success !")
+    }).catch(e => {
+        console.error("An error occurred :", e)
+    })
   }
 
-  doSearch() {
-    
-  }
-
-  clearSearch() {
-
-  }
-
-  addData() {
-    
-  }
 }
